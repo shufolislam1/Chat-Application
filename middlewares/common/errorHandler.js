f5051b44d5578ee46e3ab5c51233c30071927a0e
shufol islam <shufolislam1255@gmail.com>
2022-12-19 21:59:42 +0600

    error handler completed

--- middlewares/common/errorHandler.js
+++ middlewares/common/errorHandler.js
@@ -7,9 +7,18 @@ function notFoundHandler(req, res, next) {
 
 // default error handler
 function errorHandler(error, req, res, next) {
-    res.render("error", {
-        title: "Error Page"
-    })
+    res.locals.error = error;
+    res.status(error.status || 5000)
+
+    if(res.locals.html){
+        // html response
+        res.render("error", {
+            title: "Error page"
+        })
+    }else{
+        // json response
+        res.json(res.locals.error)
+    }
 }
 
 module.exports = {
